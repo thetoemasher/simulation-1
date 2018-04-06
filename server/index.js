dd816243f341bff7f5a2c6049bb1706aa65ccb27
Cody Tomesch <tomeschcody@gmail.com>
2018-04-06 11:30:22 -0600

    set up db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const port = 7574
+const massive = require('massive')
+require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.json());
+massive(process.env.CONNECTION_STRING).then(connection => {
+    app.set('db', connection);
+    console.log('db connected');
+})
 
 
 
